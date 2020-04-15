3324c3744ecbc509b39f766456539dadb6e2b422
Jack Madden <jck13mad@gmail.com>
2020-04-15 20:46:19 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,17 +1,16 @@
 class Board
-  
   attr_accessor :cells
-  
+
   @cells = []
-  
-  def initialize
-    self.cells = Array.new(9, " ")
-  end
-  
+
   def reset!
     self.cells = Array.new(9, " ")
   end
-  
+
+  def initialize
+    self.cells = Array.new(9, " ")
+  end
+
   def display
     puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
     puts "-----------"
@@ -19,33 +18,31 @@ class Board
     puts "-----------"
     puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
   end
-  
+
   def position(user_input)
     self.cells[user_input.to_i-1]
   end
-  
+
   def full?
     self.cells.all? {|cell| cell == "X" || cell == "O"}
   end
-  
+
   def turn_count
     self.cells.count("X") + self.cells.count("O")
   end
-  
-  def taken?(position)
-    self.cells[position.to_i-1] == "X" || self.cells[position.to_i-1] == "O"
+
+  def taken?(position) #check board position
+    self.cells[position.to_i-1] == "X" || #board position -1 because of test using range 1-9 (user input numbers)
+    self.cells[position.to_i-1] == "O"
   end
-  
+
   def valid_move?(position)
-    !taken?(position) && position.to_i-1 > 0 && position.to_i-1 <= 9
+    !taken?(position) && position.to_i >0 && position.to_i <=9
   end
-  
+
   def update(position, player)
-    self.cells[position.to_i-1] = player.token
+      self.cells[position.to_i-1] = player.token
   end
-  
-  
-  
-  
-  
+
+
 end
